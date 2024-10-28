659555697ce2ba39cabf7c0dddf9a67e74c458c6
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-10-28 19:53:06 +0500

    create product logic and saving image on cloudinary

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,4 +1,5 @@
 import { redis } from "../lib/redis.js";
+import cloudinary from "../lib/cloudinary.js";
 import Product from "../models/product.model.js";
 
 export const getAllProducts = async (req, res) => {
@@ -32,6 +33,29 @@ export const getFeaturedProducts = async (req, res) => {
 
     } catch (error) {
         console.log("Error in getFeaturedProducts controller", error);
+        res.status(500).json({ message: "server error", error: error.message });
+    }
+}
+
+export const createProduct = async (req, res) => {
+    try {
+        const {name, description, price, image, category} = req.body;
+
+        let cloudinaryResponse;
+        if(image) {
+           cloudinaryResponse = await cloudinary.uploader.upload(image, {folder: 'products'});
+        }
+
+        const product = await Product.create({
+            name,
+            description,
+            price,
+            image: cloudinaryResponse?.secure_url ? cloudinaryResponse?.secure_url : "",
+            category,
+        });
+        res.status(201).json({ product });
+    } catch (error) {
+        console.log("Error in createProduct controller", error);
         res.status(500).json({ message: "server error", error: error.message });
     }
 }
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { getAllProducts, getFeaturedProducts } from '../controllers/product.controller.js';
+import { getAllProducts, getFeaturedProducts, createProduct } from '../controllers/product.controller.js';
 import { protectRoute, adminRoute } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
 
-router.get('/', [protectRoute, adminRoute ], getAllProducts);
+router.route('/').get([protectRoute, adminRoute ], getAllProducts).post([protectRoute, adminRoute ], createProduct);
 router.get('/featured',  getFeaturedProducts);
 
 export default router;
