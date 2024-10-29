92f487a81849dc8e8d131edb0238318f68a79054
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-10-29 09:04:11 +0500

    remove products from cart route

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -20,7 +20,20 @@ export const addToCart = async (req, res) => {
 }
 
 export const removeAllProducts = async (req, res) => {
-
+    try {
+        const {productId} = req.body;
+        const user = req.user;
+        if(!productId){
+            user.cartItems = [];
+        } else {
+            user.cartItems = user.cartItems.filter((item) => item.id !== productId);
+        }
+        await user.save();
+        res.json(user.cartItems);
+    } catch (error) {
+        console.log("Error in removeAllProducts controller", error);
+        res.status(500).json({ message: "server error", error: error.message });
+    }
 }
 
 export const getCartProducts = async (req, res) => {
--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { addToCart } from '../controllers/cart.controller.js';
+import { addToCart, removeAllProducts } from '../controllers/cart.controller.js';
 import { protectRoute } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
 router.post('/', protectRoute, addToCart)
+router.delete('/', protectRoute, removeAllProducts)
 
 export default router;
