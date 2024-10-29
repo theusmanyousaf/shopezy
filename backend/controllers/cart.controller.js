8f479a976695bf8b1dc2479a4b011a01247a9776
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-10-29 09:11:10 +0500

    update product quantity in cart route

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -1,3 +1,4 @@
+import e from "express";
 
 export const addToCart = async (req, res) => {
     try {
@@ -41,5 +42,25 @@ export const getCartProducts = async (req, res) => {
 }
 
 export const updateProductQuantity = async (req, res) => {
-
+    try {
+        const productId = req.params.id;
+        const {quantity} = req.body;
+        const user = req.user;
+        const existingItem = user.cartItems.find(item => item.id === productId);
+        if(existingItem) {
+            if(quantity === 0) {
+                user.cartItems = user.cartItems.filter((item) => item.id !== productId);
+                await user.save();
+                return res.json(user.cartItems);
+            }
+            existingItem.quantity = quantity;
+            await user.save();
+            return res.json(user.cartItems);
+        } else {
+            return res.status(404).json({ message: "Product not found in cart" });
+        }
+    } catch (error) {
+        console.log("Error in updateProductQuantity controller", error);
+        res.status(500).json({ message: "server error", error: error.message });
+    }
 }
--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { addToCart, removeAllProducts } from '../controllers/cart.controller.js';
+import { addToCart, removeAllProducts, updateProductQuantity } from '../controllers/cart.controller.js';
 import { protectRoute } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
 router.post('/', protectRoute, addToCart)
 router.delete('/', protectRoute, removeAllProducts)
+router.put('/:id', protectRoute, updateProductQuantity)
 
 export default router;
