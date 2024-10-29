fcda1df12882577d4825f90c8b31cecafc24ef77
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-10-29 10:24:41 +0500

    validate coupon route

--- backend/controllers/coupon.controller.js
+++ backend/controllers/coupon.controller.js
@@ -9,3 +9,26 @@ export const getCoupon = async (req, res) => {
         res.status(500).send({ message: "server error", error: error.message });
     }
 }
+
+export const validateCoupon = async (req, res) => {
+    try {
+        const { code } = req.body;
+        const coupon = await Coupon.findOne({ code,userId: req.user._id, isActive: true });
+        if(!coupon) {
+            return res.status(404).send({ message: "coupon not found" });
+        }
+        if(coupon.expirationDate < Date.now()) {
+            coupon.isActive = false;
+            await coupon.save();
+            return res.status(404).send({ message: "coupon expired" });
+        }
+        res.json({
+            message: "coupon is valid",
+            code: coupon.code,
+            discountPercentage: coupon.discountPercentage
+        });
+    } catch (error) {
+        console.log("Error in validateCoupon controller", error);
+        res.status(500).send({ message: "server error", error: error.message });
+    }
+}
--- backend/routes/coupon.route.js
+++ backend/routes/coupon.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getCoupon } from "../controllers/coupon.controller.js";
+import { getCoupon, validateCoupon } from "../controllers/coupon.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/",protectRoute, getCoupon);
+router.get("/", protectRoute, getCoupon);
+router.get("/validate", protectRoute, validateCoupon);
 
 
 export default router;
