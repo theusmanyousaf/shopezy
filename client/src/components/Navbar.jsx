4cce5f8c851e4f0bd14f1083d6576904a6914c12
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-10-30 14:04:23 +0500

    signup page ui

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 import { ShoppingCart, UserPlus, LogIn, LogOut, Lock } from 'lucide-react';
 import { Link } from 'react-router-dom';
 const Navbar = () => {
-  const user = true;
-  const isAdmin = true;
+  const user = false;
+  const isAdmin = false;
 
   return (
     <header className='fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800'>
