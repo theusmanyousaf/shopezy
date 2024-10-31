4ca77da571412e4af0f7f405558bf93521945039
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-10-31 19:28:43 +0500

    order summary component ui

--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -4,6 +4,7 @@ import { motion } from "framer-motion";
 import { ShoppingCart } from "lucide-react";
 import CartItem from "../components/CartItem";
 import PeopleAlsoBought from "../components/PeopleAlsoBought";
+import OrderSummary from "../components/OrderSummary";
 
 const CartPage = () => {
     const { cart } = useCartStore();
@@ -29,6 +30,17 @@ const CartPage = () => {
                         )}
                         {cart.length > 0 && <PeopleAlsoBought />}
                     </motion.div>
+
+                    {cart.length > 0 && (
+                        <motion.div
+                            className='mx-auto mt-6 max-w-4xl flex-1 space-y-6 lg:mt-0 lg:w-full'
+                            initial={{ opacity: 0, x: 20 }}
+                            animate={{ opacity: 1, x: 0 }}
+                            transition={{ duration: 0.5, delay: 0.4 }}
+                        >
+                            <OrderSummary />
+                        </motion.div>
+                    )}
                 </div>
             </div>
         </div>
