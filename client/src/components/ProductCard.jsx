589b5885974c8d5bb452851323ec0b06ce36c637
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-10-31 18:38:44 +0500

    add to cart, get card items & calculate totals in useCartStore + product card ui updated

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,6 +1,20 @@
+import toast from "react-hot-toast";
 import { ShoppingCart } from "lucide-react";
+import { useUserStore } from "../stores/useUserStore";
+import { useCartStore } from "../stores/useCartStore";
 
 const ProductCard = ({ product }) => {
+	const { user } = useUserStore();
+	const { addToCart } = useCartStore();
+	const handleAddToCart = () => {
+		if (!user) {
+			toast.error("Please login to add products to cart", { id: "login" });
+			return;
+		} else {
+			// add to cart
+			addToCart(product);
+		}
+	};
 
 	return (
 		<div className='flex w-full relative flex-col overflow-hidden rounded-lg border border-gray-700 shadow-lg'>
@@ -19,6 +33,7 @@ const ProductCard = ({ product }) => {
 				<button
 					className='flex items-center justify-center rounded-lg bg-emerald-600 px-5 py-2.5 text-center text-sm font-medium
 					 text-white hover:bg-emerald-700 focus:outline-none focus:ring-4 focus:ring-emerald-300'
+					onClick={handleAddToCart}
 				>
 					<ShoppingCart size={22} className='mr-2' />
 					Add to cart
