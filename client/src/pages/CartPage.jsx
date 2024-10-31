bf9244fffc26109b3a0d7453e004e5ef049be5ac
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-10-31 19:33:54 +0500

    git coupon card ui

--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -5,6 +5,7 @@ import { ShoppingCart } from "lucide-react";
 import CartItem from "../components/CartItem";
 import PeopleAlsoBought from "../components/PeopleAlsoBought";
 import OrderSummary from "../components/OrderSummary";
+import GiftCouponCard from "../components/GiftCouponCard";
 
 const CartPage = () => {
     const { cart } = useCartStore();
@@ -39,6 +40,7 @@ const CartPage = () => {
                             transition={{ duration: 0.5, delay: 0.4 }}
                         >
                             <OrderSummary />
+                            <GiftCouponCard />
                         </motion.div>
                     )}
                 </div>
