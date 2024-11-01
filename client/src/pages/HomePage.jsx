5f063865cc07b72a9edcbd7d2d72418ed3cca972
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-11-01 14:23:06 +0500

    featured products slider on homepage + fetch featured products from database + featured prducts component ui

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,4 +1,7 @@
+import { useEffect } from "react";
 import CategoryItem from "../components/CategoryItem";
+import { useProductStore } from "../stores/useProductStore";
+import FeaturedProducts from "../components/FeaturedProducts";
 
 const categories = [
   { href: "/jeans", name: "Jeans", imageUrl: "/jeans.jpg" },
@@ -9,7 +12,14 @@ const categories = [
   { href: "/suits", name: "Suits", imageUrl: "/suits.jpg" },
   { href: "/bags", name: "Bags", imageUrl: "/bags.jpg" },
 ];
+
 const HomePage = () => {
+  const { fetchFeaturedProducts, products, isLoading } = useProductStore();
+
+  useEffect(() => {
+    fetchFeaturedProducts();
+  }, [fetchFeaturedProducts]);
+
   return (
     <div className='relative min-h-screen text-white overflow-hidden'>
       <div className='relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16'>
@@ -25,9 +35,10 @@ const HomePage = () => {
             <CategoryItem category={category} key={category.name} />
           ))}
         </div>
+
+        {!isLoading && products.length > 0 && <FeaturedProducts featuredProducts={products} />}
       </div>
     </div>
-  )
-}
-
+  );
+};
 export default HomePage;
